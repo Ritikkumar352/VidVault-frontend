f5b183f75df6c5985e298b23ce2db53d59f3a244
ritikkumar352 <krritik2002@gmail.com>
2025-03-12 18:55:18 +0530

    css

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './app.css'  // Update this import to point to your CSS file with Tailwind
+import './App.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
